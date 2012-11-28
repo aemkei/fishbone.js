b21f30e0a4c8a51e197aff119e816fdd94ba32c1
Martin Kleppe <kleppe@ubilabs.net>
2012-11-28 15:54:36 +0100

    Add line breaks to uglified output.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 uglifyjs -nc fishbone.js > fishbone.min.js
 gzip -c -f --best fishbone.min.js > fishbone.min.js.gz
+
+uglifyjs --max-line-len 78 -nc fishbone.js
+echo "// c-{{{-<"
 wc -c fishbone.min.js.gz
 wc -c fishbone.min.js
