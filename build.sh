7143519d48bc141e99fe7a5a32d5f8aafc150aff
Martin Kleppe <kleppe@ubilabs.net>
2012-11-30 12:52:44 +0100

    Small tweak in build script.

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ uglifyjs -nc fishbone.js > fishbone.min.js
 gzip -c -f --best fishbone.min.js > fishbone.min.js.gz
 
 uglifyjs --max-line-len 78 -nc fishbone.js
-echo "// c-{{{-<"
+echo " // c-{{{-<"
 wc -c fishbone.min.js.gz
 wc -c fishbone.min.js
