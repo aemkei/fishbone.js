810718e4ee0192b6beb48689e9b779fe856cefb8
Tan Li Hau <lhtan93@gmail.com>
2016-04-12 12:06:42 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-uglifyjs -nc fishbone.js > fishbone.min.js
+uglifyjs --max-line-len 78  -mc -- fishbone.js > fishbone.min.js
 gzip -c -f --best fishbone.min.js > fishbone.min.js.gz
 
-uglifyjs --max-line-len 78 -nc fishbone.js
+uglifyjs --max-line-len 78 -mc -- fishbone.js
 echo " // c-{{{-<"
 wc -c fishbone.min.js.gz
 wc -c fishbone.min.js
